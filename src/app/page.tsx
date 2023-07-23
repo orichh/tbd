3628d09bede29bc3a1c2a067105a179327832c4b
Richard Hyesung O <orichh@gmail.com>
2023-07-23 18:59:04 -0400

    it is working, yeee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,70 @@
+"use client";
+
 import Video from "@/components/Video";
+import { useState } from "react";
+
+type Task = {
+  id: number;
+  name: string;
+  isCompleted: boolean;
+};
+type UserDay = {
+  id: number;
+  userId: number;
+  date: Date | string;
+  tasks: Task[];
+};
+
+type User = {
+  id: number;
+  username: string;
+};
+
+const today = new Date();
+const sampleTasks: Task[] = [
+  { id: 1, name: "eat breakfast", isCompleted: false }
+];
+const sampleData: UserDay[] = [
+  {
+    id: 1,
+    userId: 1,
+    date: today.toISOString().slice(0, 10),
+    tasks: sampleTasks
+  }
+];
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+  const [tasks, setTasks] = useState(sampleTasks);
+
+  const handleUpdateTask = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    index: number
+  ) => {
+    const oldTasks = [...tasks];
+    const updatedTask = oldTasks[index];
+    updatedTask.isCompleted = !oldTasks[index].isCompleted;
+
+    oldTasks[index] = updatedTask;
+    setTasks(oldTasks);
+  };
+
   return (
     <div className="flex w-full flex-col">
-      <div className="flex h-screen justify-center">
-        <Video source="/redcamp.mp4" />
+      <div className="flex h-screen flex-col">
+        <h1>tasks</h1>
+        <ul>
+          {tasks.map((task, i) => {
+            return (
+              <div key={i}>
+                <li>
+                  {task.name} - {task.isCompleted.toString()}
+                </li>
+                <button onClick={(e) => handleUpdateTask(e, i)}>update</button>
+              </div>
+            );
+          })}
+        </ul>
       </div>
 
       <p className="break-all"></p>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,13 +13,7 @@ export default function NavBar() {
           href={routes.HOME}
           className="cursor-pointer whitespace-nowrap w-52 sm:w-60 flex items-center pt-1.5"
         >
-          <Image
-            src="/logo-top.svg"
-            width={100}
-            height={100}
-            alt="test"
-            className="h-full w-full object-cover object-center -my-10"
-          />
+          <h1>tbd</h1>
         </Link>
       </div>
 
@@ -31,62 +25,6 @@ export default function NavBar() {
           >
             About
           </Link>
-        </div>
-
-        <div className="flex-1">
-          <Link
-            href={routes.PHOTOS}
-            className="flex cursor-pointer justify-center"
-          >
-            Photos
-          </Link>
-        </div>
-
-        {/* <div className="flex-1">
-          <Link
-            href={routes.HISTORY}
-            className="flex cursor-pointer justify-center"
-          >
-            History
-          </Link>
-        </div> */}
-
-        <div className="flex-1 whitespace-nowrap px-2 pr-4">
-          <Link
-            href={routes.WHAT_TO_DO}
-            className="flex cursor-pointer justify-center"
-          >
-            What to Do
-          </Link>
-        </div>
-
-        <div className="flex-1 pr-4">
-          <Link
-            href={routes.CONTACTS}
-            className="flex cursor-pointer justify-center"
-          >
-            Contacts
-          </Link>
-        </div>
-
-        <div className="flex-1">
-          <div className="flex border-2 border-gray-400 w-18 h-8 hover:border-green-500 hover:text-green-500">
-            <a
-              href={routes.BOOK_NOW}
-              className="flex w-full h-full cursor-pointer items-center align-middle justify-center"
-            >
-              Book
-            </a>
-          </div>
-        </div>
-
-        <div className="flex-1">
-          <a
-            href={routes.INSTAGRAM}
-            className="flex cursor-pointer justify-center"
-          >
-            <Instagram className="fill-gray-700 hover:fill-green-500" />
-          </a>
         </div>
 
         <Hamburger className="flex flex-1 align-middle items-center justify-center h-full cursor-pointer border-l-[1px] border-gray-200" />
