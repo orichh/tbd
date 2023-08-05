e081c7d22beb732fb0d51d8b48fb60839807f209
Richard Hyesung O <orichh@gmail.com>
2023-08-05 00:20:37 -0400

    add types and get_user endpoint

---
+++ src/util/types.ts
@@ -0,0 +1,34 @@
+export type Task = {
+  id: number;
+  name: string;
+  created_at: Date;
+  is_completed: boolean;
+  time_spent_in_minutes: number;
+  completed_at: Date;
+  notes: string;
+  display_order: number;
+  user_id: number;
+  associated_day_id: number;
+};
+
+export type User = {
+  id: number;
+  updated_at: Date;
+  username: string;
+  full_name: string;
+  avatar_url: string;
+  win_streak_longest: number;
+  win_streak_current: number;
+  lose_streak_longest: number;
+  lose_streak_current: number;
+};
+
+export type UsersDay = {
+  id: number;
+  user_id: number;
+  date: Date;
+  total_tasks: number;
+  total_tasks_completed: number;
+  all_tasks_completed: boolean;
+  tasks: Task[];
+};
