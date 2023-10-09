47e348144b2ec6e4dac770b18d59ebe945cadefb
Richard Hyesung O <orichh@gmail.com>
2023-10-08 22:30:06 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images: {
+    domains: ["image.tmdb.org", "xvxusgxxvghujjuuccav.supabase.co"]
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    // https://stackoverflow.com/questions/51791163/warning-prop-classname-did-not-match-when-using-styled-components-with-seman
+    styledComponents: true
+  }
+};
